4f43fac9259746f8636781715c6a3ab2aff2839e
Beaq15 <91021012+Beaq15@users.noreply.github.com>
2022-01-21 20:21:58 +0200

    Add files via upload

---
+++ Assets/IKLookAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKLookAt : MonoBehaviour
+{
+    public Transform target;
+    public float weight = 1f;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetLookAtPosition(target.position);
+        anim.SetLookAtWeight(weight);
+    }
+}
---
+++ Assets/IKPuppet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKPuppet : MonoBehaviour
+{
+    public Transform target;
+    Animator anim;
+    float weight = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPosition(AvatarIKGoal.RightFoot, target.position);
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, weight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            target.transform.Translate(0f, 0.1f, 0f);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            target.transform.Translate(0f, -0.1f, 0f);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            target.transform.Translate(0.1f, 0f, 0f);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            target.transform.Translate(-0.1f, 0f, 0f);
+    }
+}
